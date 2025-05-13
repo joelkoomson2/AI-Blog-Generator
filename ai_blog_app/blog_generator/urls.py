7d72fd643496fb1c50bf725876345e7ec2c81686
Your Name <dashingallthewaysource@gmail.com>
2025-05-13 11:57:41 -0400

    fifth commit

--- ai_blog_app/blog_generator/urls.py
+++ ai_blog_app/blog_generator/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views  # Import your views here 
 
 urlpatterns = [
-    path('', views.index, name= 'index') # Add your URL patterns here
+    path('', views.index, name= 'index'),# Add your URL patterns here
+    path('login', views.user_login, name= 'login'),# Add your URL patterns here
+    path('signup', views.user_signup, name= 'signup') ,# Add your URL patterns here
+    path('logout', views.user_logout, name= 'logout'),# Add your URL patterns here
 ]
--- ai_blog_app/blog_generator/views.py
+++ ai_blog_app/blog_generator/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html') # Render the index.html template to return to the homepage 
+
+def user_login(request):
+    return render(request, 'login.html') # Render the login.html template to return to the login page
+
+def user_signup(request):
+    return render(request, 'signup.html') # Render the signup.html template to return to the signup page
+
+def user_logout(request):
+    pass
